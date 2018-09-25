a4f9047d6669b91372af782a3f6f5dd8a4342daa
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-25 22:06:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
   
   if phrase=="I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else if phrase==phrase.
+  else if phrase==phrase.upcase
     
   
 end
